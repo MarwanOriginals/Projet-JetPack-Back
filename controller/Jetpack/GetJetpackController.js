5b58175a0faa8984355acf33a48a44a15225e99a
MARWAN <mwgg@sfr.fr>
2019-10-10 16:53:41 +0200

    correction

--- controller/Jetpack/GetJetpackController.js
+++ controller/Jetpack/GetJetpackController.js
@@ -1,4 +1,16 @@
+const Jetpack = require("../../src/Entity/Jetpack");
+const db = require('../../src/Db');
+const JetpackRepository = require('../../src/Repository/JetpackRepository');
+
 module.exports = (req, res) => {
-    var id = req.params.id;
-    res.send('Not implemented');
+    let id = req.body.id;
+    const repository = new JetpackRepository(db);
+    let jetpacks;
+    if (id === undefined)
+        jetpacks = repository.getAll();
+    else
+        jetpacks = repository.getOne(id);
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(200).send(jetpacks)
 };
