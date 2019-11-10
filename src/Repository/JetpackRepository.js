32858caa7d8245509b8bbc5ecbab87e6d7584257
MagicMike0 <mike.malecot@yahoo.fr>
2019-11-10 22:37:12 +0100

    getAvailableJetPacks + tests

--- src/Repository/JetpackRepository.js
+++ src/Repository/JetpackRepository.js
@@ -17,16 +17,36 @@ module.exports = class {
             .push(jetpack.toJson())
             .write()
     }
-    // Marwan
+
     getAll() {
         return this.db.get('jetpacks').value();
     }
 
-    // Marwan
     getOne(Id) {
         if (this.db.get('jetpacks').filter({id: Id}).size().value() < 1)
             throw 'Jetpack with ID :' + Id + ' was not founded';
 
         return this.db.get('jetpacks').filter({id: Id}).value();
     }
+
+	getAvailable(start, end) {
+		let startDate = new Date(start);
+		let endDate = new Date(end);
+		console.log(startDate);
+		if( isNaN(startDate) )
+            throw 'startDate is not valid';
+		if( isNaN(endDate) )
+            throw 'endDate is not valid';
+		if( startDate > endDate )
+            throw 'end date should be greater than start date';
+
+		let notAvailables = this.db.get('bookings').value()
+		.filter( booking => new Date(booking.startDate) <= endDate && new Date(booking.endDate) >= startDate )
+		.map( booking => booking.jetpackId);
+		
+		let availables = this.db.get('jetpacks').value()
+		.filter( jetpack => !(notAvailables.includes(jetpack.id)) );
+
+		return availables;
+	}
 };
